8bc65ee6bf3705abfbf60cab5ad1daff461cb066
Weiner <woliveira82@gmail.com>
2019-09-27 23:08:24 -0300

    CSS corrections and bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className='row'>
-        <div className='col'>
-          <div className='config'>
-            <div className="row">
-              <div className="col-lg-4 col-md-6 col-sm-12 center-block">
-                <form className="form-signin">
-                  <div className="form-label-group">
-                    <div className="row">
-                      <div className="col text-center">
-                      </div>
+    return (
+        <div className="App">
+            <div className='row'>
+                <div className='col'>
+                    <div className='config'>
+                        <div className="row">
+                            <div className="col-lg-4 col-md-6 col-sm-12 center-block">
+                                <form className="form-signin">
+                                    <div className="form-label-group">
+                                        <div className="row">
+                                            <div className="col text-center">
+                                                <img id="SWLogo" src="/img/SW_LOGO_24bit.png" alt="Savage Worlds Logo"/>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col text-center">
+                                                Scenario
+                                                <select name="scenario" className="form-control">
+                                                    <option value="" disabled selected>Choose a scenario...</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col text-center">
+                                                Character
+                                                <select name="character" className="form-control">
+                                                    <option value="" disabled selected>Choose a character...</option>
+                                                    <option value="newchar">Create a new character</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col text-center">
+                                                <button className="btn btn-lg btn-primary btn-block" type="submit">Play</button>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col text-center">
+                                                <button className="btn btn-lg btn-secondary btn-block" type="submit">Configuration</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div className="row">
-                      <div className="col text-center">
-                        Choose a scenario
-                              <select name="scenario" className="form-control">
-                          <option value="" disabled selected>Choose a scenario...</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col text-center">
-                        Choose a character
-                                  <select name="character" className="form-control">
-                          <option value="" disabled selected>Choose a character...</option>
-                          <option value="newchar">Create a new character</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col text-center">
-                        <button className="btn btn-lg btn-primary btn-block" type="submit">Play</button>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col text-center">
-                        <button className="btn btn-lg btn-secondary btn-block" type="submit">Configuration</button>
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
